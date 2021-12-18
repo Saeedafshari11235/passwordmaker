2f44a2cefa911ac4389fcf74bc2c094aace9bf12
Saeedafshari11235 <msafshari11235@gmail.com>
2021-12-18 18:45:04 +0330

    add hide function

--- ex_hide_extract.py
+++ ex_hide_extract.py
@@ -1,8 +1,9 @@
 import patoolib
-
+import os
 
 def extract(fileName , fileDest):
     patoolib.extract_archive(fileName, outdir=fileDest)
 
-
+def hide(hidedFile):
+    os.system("attrib +h " + hidedFile)
 
