c02dba639a161c6d7eec5d632feab7953926281b
Saeedafshari11235 <msafshari11235@gmail.com>
2021-12-21 15:47:44 +0330

    something

--- ex_hide_extract.py
+++ ex_hide_extract.py
@@ -19,4 +19,5 @@ def key ():
    message_bytes = message.encode('ascii')
    base64_bytes = base64.b64encode(message_bytes)
    base64_message = base64_bytes.decode('ascii')
-   print (base64_message)
+   return base64_message
+
