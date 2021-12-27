29742336efd0c429d936e0abdb2852488eade4fc
Saeedafshari11235 <msafshari11235@gmail.com>
2021-12-27 14:32:49 +0330

    complete the program

--- ex_hide_extract.py
+++ ex_hide_extract.py
@@ -1,27 +1,25 @@
 import patoolib
 import os
-import uuid
-import base64
-
 
 def extract (filepath:str , extractdir:str ):
    desname = "saw"
    if not os.path.isdir(extractdir + "/"+ desname):
       os.chdir(extractdir)
       os.mkdir(desname)
-      #patoolib.extract_archive(filepath, outdir = extractdir+desname)
-      os.system( "tar xvf " + filepath +" -C "+ extractdir + "/"+ desname)
+      patoolib.extract_archive(filepath, outdir = extractdir + "/"+ desname)
+      #os.system( "tar xvf " + filepath +" -C "+ extractdir + "/"+ desname)
+      hide()
    else:
       os.remove(extractdir +"/"+ desname)
-      extract()
+      extract(extractdir + "/"+ desname)
 
 def hide (path:str):
    os.system( "attrib +h " + path) 
 
-def key ():
-   message = str(uuid.getnode())
-   message_bytes = message.encode('ascii')
-   base64_bytes = base64.b64encode(message_bytes)
-   base64_message = base64_bytes.decode('ascii')
-   return base64_message
 
+print("!Hider,extracter!")
+print("Enter the path of file that you wanna extract:(exm:C:/my/folder/zip.zip)")
+z = input()
+print("Enter the path that you wanna extract the file into:(exm:C:/my/folder/)")
+y = input()
+extract(z,y)
